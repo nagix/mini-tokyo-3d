d0b1172224765cc3911c67ad7a5ce9e7656625bc
Akihiko Kusanagi <nagi@nagi-p.com>
2019-12-17 19:46:31 +0800

    Update train timetables

--- js/main.js
+++ js/main.js
@@ -83,7 +83,7 @@ var realtimeTrainLookup = {};
 var flightLookup = {};
 var activeFlightLookup = {};
 var animationID = 0;
-var lastStaticUpdate = '2019-11-28 10:00:00';
+var lastStaticUpdate = '2019-12-16 10:00:00';
 var lastDynamicUpdate = {};
 var stationLookup, stationTitleLookup, railwayLookup, railDirectionLookup, trainTypeLookup, trainLookup, operatorLookup, airportLookup, a;
 var trackedObject, markedObject, lastTimetableRefresh, lastTrainRefresh, lastFrameRefresh, trackingBaseBearing, viewAnimationID, layerZoom, altitudeUnit, objectUnit, objectScale, carScale, aircraftScale;
