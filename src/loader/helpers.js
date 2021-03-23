dc5ca5669a4374dd1c17063453cb7cabd6d9e5e4
Akihiko Kusanagi <nagi@nagi-p.com>
2021-03-23 18:14:09 +0800

    Update Tsukuba Express and Sotetsu timetables

--- src/loader/helpers.js
+++ src/loader/helpers.js
@@ -4,23 +4,25 @@ import pako from 'pako';
 
 export function loadJSON(url) {
     return new Promise((resolve, reject) => {
-        if (url.startsWith('https')) {
-            https.get(url, res => {
-                let body = '';
+        setTimeout(() => {
+            if (url.startsWith('https')) {
+                https.get(url, res => {
+                    let body = '';
 
-                res.setEncoding('utf8');
-                res.on('data', chunk => {
-                    body += chunk;
-                });
-                res.on('end', () => resolve(JSON.parse(body)));
-            }).on('error', error => reject(error));
-        } else {
-            fs.promises.readFile(url).then(
-                data => resolve(JSON.parse(data))
-            ).catch(
-                error => reject(error)
-            );
-        }
+                    res.setEncoding('utf8');
+                    res.on('data', chunk => {
+                        body += chunk;
+                    });
+                    res.on('end', () => resolve(JSON.parse(body)));
+                }).on('error', error => reject(error));
+            } else {
+                fs.promises.readFile(url).then(
+                    data => resolve(JSON.parse(data))
+                ).catch(
+                    error => reject(error)
+                );
+            }
+        }, 0);
     });
 }
 
