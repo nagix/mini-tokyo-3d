ed9540ba8259d67a6600ad03f56e2e5af32d8a9d
Akihiko Kusanagi <nagi@nagi-p.com>
2023-01-05 21:19:37 +0800

    Support for displaying information on barrier-free facilities at stations

--- src/station-panel.js
+++ src/station-panel.js
@@ -1,4 +1,5 @@
 import Panel from './panel';
+import {includes} from './helpers';
 
 export default class extends Panel {
 
@@ -11,25 +12,36 @@ export default class extends Panel {
             exitHTML = [],
             stations = me._options.object,
             titles = {},
-            exits = [].concat(...stations.map(station => station.exit || []));
+            exits = [].concat(...stations.map(station => station.exit || [])),
+            {clock, dict, container} = map,
+            now = clock.getTime();
 
         for (const {id} of stations) {
             titles[map.getLocalizedStationTitle(id)] = true;
         }
 
         for (const id of exits) {
+            const poi = map.poiLookup[id],
+                calendar = clock.getCalendar(),
+                uptime = poi.uptime && poi.uptime.reduce((acc, val) => !val.calendar || includes(val.calendar, calendar) ? val : acc, {}),
+                closed = uptime && (now < clock.getTime(uptime.open) || now >= clock.getTime(uptime.close) || uptime.open === uptime.close);
+
             exitHTML.push([
-                '<div class="exit-row">',
+                `<div class="exit-row${closed ? ' closed' : ''}">`,
+                '<div class="exit-icon-box"></div>',
                 '<div class="exit-title-box">',
                 map.getLocalizedPOIDescription(id),
+                uptime && uptime.open !== uptime.close ? ` (${uptime.open}-${uptime.close})` : '',
                 '</div>',
+                includes(poi.facilities, 'elevator') ? '<div class="exit-elevator-icon"></div>' : '',
+                includes(poi.facilities, 'escalator') ? '<div class="exit-escalator-icon"></div>' : '',
                 '<div class="exit-share-button"></div>',
                 '</div>'
             ].join(''));
         }
 
         super.addTo(map)
-            .setTitle(Object.keys(titles).join(map.dict['and']))
+            .setTitle(Object.keys(titles).join(dict['and']))
             .setHTML(exitHTML.join(''));
 
         const {children} = me._container.querySelector('#panel-content');
@@ -37,15 +49,22 @@ export default class extends Panel {
         for (let i = 0, ilen = children.length; i < ilen; i++) {
             const child = children[i];
 
+            child.addEventListener('click', () => {
+                map.map.flyTo({
+                    center: map.poiLookup[exits[i]].coord,
+                    zoom: 19,
+                    pitch: 30
+                });
+            });
             child.addEventListener('mouseenter', () => {
-                const element = map.container.querySelector(`#exit-${i}`);
+                const element = container.querySelector(`#exit-${i}`);
 
                 if (element) {
                     element.classList.add('highlighted');
                 }
             });
             child.addEventListener('mouseleave', () => {
-                const element = map.container.querySelector(`#exit-${i}`);
+                const element = container.querySelector(`#exit-${i}`);
 
                 if (element) {
                     element.classList.remove('highlighted');
