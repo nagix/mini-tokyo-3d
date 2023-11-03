95dbaf54afc33fd160905aa586d7e1f4e3649372
Akihiko Kusanagi <nagi@nagi-p.com>
2023-11-03 18:02:52 +0800

    Update Ryutetsu Nagareyama line timetables

--- src/configs.js
+++ src/configs.js
@@ -123,7 +123,7 @@ const configs = {
     searchUrl: 'https://search.minitokyo3d.com/api/v1/routes',
 
     // Timestamp when the static data was last updated
-    lastStaticUpdate: '2023-04-01 04:00:00',
+    lastStaticUpdate: '2023-07-01 03:00:00',
 
     // String to show in an Mapbox's AttributionControl
     customAttribution: '<a href="https://github.com/nagix/mini-tokyo-3d">© Akihiko Kusanagi</a>',
