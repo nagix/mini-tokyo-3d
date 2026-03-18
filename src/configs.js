c708abea8bf05ab56137a50e52c032112b6f9beb
Akihiko Kusanagi <nagi@nagi-p.com>
2026-03-18 15:47:11 +0900

    Revise timetables according to the schedule revision on March 14

--- src/configs.js
+++ src/configs.js
@@ -155,7 +155,7 @@ const configs = {
     searchUrl: 'https://search.minitokyo3d.com/api/v1/routes',
 
     // Timestamp when the static data was last updated
-    lastStaticUpdate: '2025-12-20 03:00:00',
+    lastStaticUpdate: '2026-03-16 09:00:00',
 
     // String to show in an Mapbox's AttributionControl
     customAttribution: '<a href="https://github.com/nagix/mini-tokyo-3d">© Akihiko Kusanagi</a>',
